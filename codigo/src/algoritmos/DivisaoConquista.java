6c77fccd2dc1112ffa56df7514162f4ddae60c36
Lucas Soares <polonio230@hotmail.com>
2024-06-23 19:23:59 -0300

    refactor: removendo comentários

--- codigo/src/algoritmos/DivisaoConquista.java
+++ codigo/src/algoritmos/DivisaoConquista.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class DivisaoConquista {
 
     public static List<Lance> resolver(List<Lance> lances, int energiaMaxima) {
-        // Ordenar os lances por valor por unidade de energia em ordem decrescente
+
         lances.sort((l1, l2) -> Double.compare((double) l2.valor / l2.energia, (double) l1.valor / l1.energia));
         List<Lance> lancesSelecionados = new ArrayList<>();
         divisaoConquista(lances, 0, lances.size() - 1, energiaMaxima, lancesSelecionados);
@@ -19,7 +19,7 @@ public class DivisaoConquista {
             return;
         }
 
-        // Encontrar o lance que maximiza o valor por unidade de energia
+
         int indiceMaximoLucro = esquerda;
         for (int i = esquerda + 1; i <= direita; i++) {
             if ((double) lances.get(i).valor / lances.get(i).energia > (double) lances.get(indiceMaximoLucro).valor / lances.get(indiceMaximoLucro).energia) {
@@ -29,13 +29,13 @@ public class DivisaoConquista {
 
         Lance lanceSelecionado = lances.get(indiceMaximoLucro);
 
-        // Se o lance cabe na energia disponível
+
         if (lanceSelecionado.energia <= energiaRestante) {
             lancesSelecionados.add(lanceSelecionado);
             energiaRestante -= lanceSelecionado.energia;
         }
 
-        // Resolver os subproblemas recursivamente
+
         divisaoConquista(lances, esquerda, indiceMaximoLucro - 1, energiaRestante, lancesSelecionados);
         divisaoConquista(lances, indiceMaximoLucro + 1, direita, energiaRestante, lancesSelecionados);
     }
